6d98f86e24b307605f9575a240443ab3d98f8738
matikkk231 <115667337+matikkk231@users.noreply.github.com>
2023-02-03 15:25:02 +0300

    add: secondModeOfTheCamera

--- GameJamUnity/Assets/Project/Scripts/ChasingCamera.cs
+++ GameJamUnity/Assets/Project/Scripts/ChasingCamera.cs
@@ -2,10 +2,32 @@ using UnityEngine;
 
 public class ChasingCamera : MonoBehaviour
 {
+    private int _currentCameraMode = 1;
+
     [SerializeField] private Transform _target;
 
     void Update()
     {
-        transform.position = new Vector3(_target.transform.position.x, _target.transform.position.y, -10);
+        if (_currentCameraMode == 1)
+        {
+            transform.position = new Vector3(_target.transform.position.x, _target.transform.position.y, -10);
+        }
+
+        if (_currentCameraMode == 2)
+        {
+            var transform1 = transform;
+            transform1.position = new Vector3(_target.transform.position.x, _target.transform.position.y + 2, -3);
+            transform1.rotation = Quaternion.Euler(30, 0, 0);
+        }
+    }
+
+    private void ChangeCameraModeToSecondPhase()
+    {
+        _currentCameraMode = 2;
+    }
+
+    private void ChangeCameraModeToFirstPhase()
+    {
+        _currentCameraMode = 1;
     }
 }
