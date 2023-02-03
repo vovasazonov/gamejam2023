3afe45b2958a37c5f1935359420b97573a3e198c
matikkk231 <115667337+matikkk231@users.noreply.github.com>
2023-02-03 14:12:05 +0300

    add: AudioManager

---
+++ GameJamUnity/Assets/Project/Scripts/AudioMananger/AudioManager.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using Project.Scripts.AudioMananger;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private static AudioManager _audioManager;
+    [SerializeField] private AudioSourceObject _audioSourcePrefab;
+
+    public List<SoundByType> Clips;
+
+    public static AudioManager Instance => _audioManager;
+
+    private void Awake()
+    {
+        if (_audioManager == null)
+        {
+            _audioManager = this;
+        }
+    }
+
+    [Serializable]
+    public struct SoundByType
+    {
+        public Sound Sound;
+        public AudioClip Clip;
+    }
+
+    public enum Sound
+    {
+        FirstPhaseBackgroundMusic
+    }
+
+    public void PlayAudio(Sound sound)
+    {
+        AudioClip audioClip = null;
+        foreach (var element in Clips)
+        {
+            if (element.Sound == sound)
+            {
+                audioClip = element.Clip;
+            }
+        }
+
+        var audioSource = Instantiate(_audioSourcePrefab);
+        audioSource.PlaySound(audioClip);
+    }
+}
---
+++ GameJamUnity/Assets/Project/Scripts/AudioMananger/AudioSourceObject.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Project.Scripts.AudioMananger
+{
+    public class AudioSourceObject : MonoBehaviour
+    {
+        private bool _isStartedPlaying;
+        [SerializeField] private AudioSource _audioSourceComponent;
+
+        public void PlaySound(AudioClip audioClip)
+        {
+            _audioSourceComponent.clip = audioClip;
+            _audioSourceComponent.Play();
+            _isStartedPlaying = true;
+        }
+
+        private void Update()
+        {
+            if (_isStartedPlaying)
+            {
+                if (!_audioSourceComponent.isPlaying)
+                {
+                    Destroy(this);
+                }
+            }
+        }
+    }
+}
