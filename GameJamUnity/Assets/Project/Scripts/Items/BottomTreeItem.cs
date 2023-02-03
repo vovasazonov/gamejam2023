cd85ad894bc8877e608f12ffadc384c88acf0d9b
Vladimir <48253536+vovasazonov@users.noreply.github.com>
2023-02-03 13:00:22 +0200

    add: root logic

--- GameJamUnity/Assets/Project/Scripts/Items/BottomTreeItem.cs
+++ GameJamUnity/Assets/Project/Scripts/Items/BottomTreeItem.cs
@@ -1,10 +1,18 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+
 namespace Project.Scripts.Items
 {
     public class BottomTreeItem : Item
     {
+        [SerializeField] private RootItem _rootItemPrefab;
+        [FormerlySerializedAs("_playerType")] [SerializeField] private PlayerTypeComponent _playerThatRelatedTo;
+        
         protected override void OnAction(PlayerType playerType)
         {
-            throw new System.NotImplementedException();
+            var rootItem = Instantiate(_rootItemPrefab);
+            rootItem.SetFollowStartRoot(transform);
+            rootItem.SetFollowEndRoot(_playerThatRelatedTo.transform);
         }
     }
 }
--- GameJamUnity/Assets/Project/Scripts/Items/RootItem.cs
+++ GameJamUnity/Assets/Project/Scripts/Items/RootItem.cs
@@ -1,10 +1,35 @@
+using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+
 namespace Project.Scripts.Items
 {
     public class RootItem : Item
     {
+        [FormerlySerializedAs("_startRoot")] public Transform _startRootTransform;
+        [FormerlySerializedAs("_endRoot")] public Transform _endRootTransform;
+        private Transform _followStartTransform;
+        private Transform _followEndTransform;
+
         protected override void OnAction(PlayerType playerType)
         {
             throw new System.NotImplementedException();
+        }
+
+        public void SetFollowStartRoot(Transform transform)
+        {
+            _followStartTransform = transform;
+        }
+
+        public void SetFollowEndRoot(Transform transform)
+        {
+            _followEndTransform = transform;
+        }
+
+        private void FixedUpdate()
+        {
+            _startRootTransform.position = _followStartTransform.position;
+            _endRootTransform.position = _followEndTransform.position;
         }
     }
 }
