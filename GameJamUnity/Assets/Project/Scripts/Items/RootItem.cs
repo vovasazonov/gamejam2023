34e84e43517047f11d1ecb046e7692eef6dc8382
Vladimir <48253536+vovasazonov@users.noreply.github.com>
2023-02-03 11:35:41 +0200

    add: items

---
+++ GameJamUnity/Assets/Project/Scripts/Items/RootItem.cs
@@ -0,0 +1,10 @@
+namespace Project.Scripts.Items
+{
+    public class RootItem : Item
+    {
+        protected override void OnAction(PlayerType playerType)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
